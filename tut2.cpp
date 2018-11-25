65d08d043975c6cb15cc67071a3f752188f81327
kgadgil <kgadgil@github.com>
2018-11-24 20:19:22 -0500

    two threads declared separately and run sometimes overlay output but mostly correct.

--- tut2.cpp
+++ tut2.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <thread>
 
+template<typename Type>
+void threadFunc () {
+	std::cout << "Type is => " << typeid(Type).name() <<std::endl;
+}
+
+
 int main (int argc, char* argv[]){
 	int NUMTHREADS = std::stoi(argv[1]);
 	std::cout << "num threads " << NUMTHREADS << std::endl;
 
-	std::thread threadArr[NUMTHREADS];
+	//std::thread threadArr[NUMTHREADS];
 
-	for (int i = 0; i < NUMTHREADS; i++){
+	/***************************************/
+	//LAMBDA FUNC TO PRINT THREADID OF ARRAY OF THREADS 
+	/***************************************/
+	/*for (int i = 0; i < NUMTHREADS; i++){
 		threadArr[i] = std::thread{
 			[&](){
 				std::cout << "threadid " << std::this_thread::get_id() << std::endl;
 			}
 		};
-	}
+	}*/
 
-	for (int i =0; i < NUMTHREADS; i++){
+	std::thread t1 {threadFunc<int>};
+	std::thread t2 {threadFunc<float>};
+
+	t1.join();
+	t2.join();
+	/*for (int i =0; i < NUMTHREADS; i++){
 		threadArr[i].join();
-	}
+	}*/
 	return 0;
 }
