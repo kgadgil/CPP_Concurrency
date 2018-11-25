4ae99b6528ba314ca4f79e85421fb426335c06c8
kgadgil <kgadgil@github.com>
2018-11-24 22:07:24 -0500

    templating for init_list and vector

--- tut2.cpp
+++ tut2.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <thread>
+#include <initializer_list>
+#include <vector>
 
-template<typename Type>
-void threadFunc () {
-	std::cout << "Type is => " << typeid(Type).name() <<std::endl;
+template<typename T>
+void printme (T&& t) {
+	std::cout << "Type is => " << typeid(T).name() <<std::endl;
+	for(auto i : t) {
+		std::cout << i;
+	}
+	std::cout << std::endl;
 }
 
 
@@ -24,11 +30,26 @@ int main (int argc, char* argv[]){
 		};
 	}*/
 
-	threadArr[0] = std::thread {threadFunc<int>};
-	threadArr[1] = std::thread {threadFunc<float>};
 
-	for (int i =0; i < NUMTHREADS; i++){
+	/***************************************/
+	//PASSING TEMPLATE FUNCTION 
+	/***************************************/
+
+	//threadArr[0] = std::thread {threadFunc<int>};
+	//threadArr[1] = std::thread {threadFunc<float>};
+
+
+	/***************************************/
+	//INITIALIZER LIST 
+	/***************************************/
+
+	std::initializer_list<int> il = {1,2,3};
+	std::vector<int> vec = {4,5,6};
+	printme(il);
+	printme(vec);
+
+	/*for (int i =0; i < NUMTHREADS; i++){
 		threadArr[i].join();
-	}
+	}*/
 	return 0;
 }
