c18633c1a34260bfa5af4a21cd98e35f8d8639f5
kgadgil <kgadgil@github.com>
2018-11-26 17:08:56 -0500

    tut5: pass class method call to threadl list_mutex: pass func to thread, global list,mutex

--- list_mutex.cpp
+++ list_mutex.cpp
@@ -7,27 +7,36 @@
 #include <mutex>
 #include <algorithm>
 
-std::list<int> some_list;
-std::mutex some_mutex;
+//class ConcList {
+//public:
+	std::list<int> some_list;
+	std::mutex some_mutex;
 
-void add_to_list(int val) {
-	std::lock_guard<std::mutex> guard(some_mutex);
-	some_list.push_back(val);
-	std::cout << "pushed value " << val << " to list" << std::endl;
-	std::cout << "locked by thread" << std::this_thread::get_id() << std::endl;
-}
+	void add(int val) {
+		std::lock_guard<std::mutex> guard(some_mutex);
+		some_list.push_back(val);
+		std::cout << "pushed value " << val << " to list" << std::endl;
+		std::cout << "locked by thread" << std::this_thread::get_id() << std::endl;
+	}
+	void remove(){
+		std::lock_guard<std::mutex> guard(some_mutex);
+		some_list.pop_back();
+		std::cout << "popped a value " << std::endl;
+		std::cout << "locked by thread" << std::this_thread::get_id() << std::endl;
+	}
+	void contains(int val){
+		std::lock_guard<std::mutex> guard(some_mutex);
+		bool found = std::find(some_list.begin(),some_list.end(), val) != some_list.end();
+		if (found){
+			std::cout << "found value " << val << std::endl;	
+		}
+		else {
+			std::cout << "not found value " << val << std::endl;
+		}
+		std::cout << "locked by thread" << std::this_thread::get_id() << std::endl;
+	}
 
-void list_contains(int val){
-	std::lock_guard<std::mutex> guard(some_mutex);
-	bool found = std::find(some_list.begin(),some_list.end(), val) != some_list.end();
-	if (found){
-		std::cout << "found value " << val << std::endl;	
-	}
-	else {
-		std::cout << "not found value " << val << std::endl;
-	}
-	std::cout << "locked by thread" << std::this_thread::get_id() << std::endl;
-}
+//};
 
 
 int main (int argc, char* argv[]){
@@ -36,8 +45,9 @@ int main (int argc, char* argv[]){
 	std::thread threadArr[NUMTHREADS];
 
 	for (int i =0; i < NUMTHREADS; i++){
-		threadArr[i] = std::thread{add_to_list, i};
+		threadArr[i] = std::thread{add, i};
 	}
+
 	for (int i =0; i < NUMTHREADS; i++){
 		threadArr[i].join();
 	}
--- tut5.cpp
+++ tut5.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
 
 class bar {
 public:
-  void foo() {
-    std::cout << "hello from member function" << std::endl;
+  void foo(std::vector<int> &vec) {
+  	for (std::vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i){
+    	std::cout << *i << ' ';
+	}std::cout << std::endl;
   }
 };
 
 int main()
 {
-  std::thread t(&bar::foo, bar());
-  t.join();
+	std::vector<int> vec = {1,2,3};
+  	std::thread t(&bar::foo, bar(), ref(vec));
+	t.join();
 }
