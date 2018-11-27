72a18870c9746b7580c9558bd781f587211fa713
kgadgil <kgadgil@github.com>
2018-11-27 00:14:41 -0500

    way to access member fun in thread

--- tut5.cpp
+++ tut5.cpp
@@ -12,9 +12,27 @@ public:
   }
 };
 
+class DummyClass {
+public:
+    DummyClass()
+    {}
+    DummyClass(const DummyClass & obj)
+    {}
+    void sampleMemberFunction(int x)
+    {
+        std::cout<<"Inside sampleMemberFunction "<<x<<std::endl;
+    }
+};
+
 int main()
 {
 	std::vector<int> vec = {1,2,3};
   	std::thread t(&bar::foo, bar(), ref(vec));
 	t.join();
+
+	DummyClass dummyObj;
+	int x = 10;
+	std::thread threadObj(&DummyClass::sampleMemberFunction, &dummyObj, x);
+	threadObj.join();
+	return 0;
 }
