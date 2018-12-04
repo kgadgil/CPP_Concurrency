d6c51f06f4f4bdc3501cdeaffbce882c4e892af9
kgadgil <kgadgil@github.com>
2018-12-03 23:54:16 -0500

    deleted, trivially modified files

--- pinch_q.cpp
+++ pinch_q.cpp
@@ -9,18 +9,32 @@ whip one up!
 #include <thread>
 #include <list>
 #include <mutex>
+#include <condition_variable>
 
 template<typename T>
-class concurrent_queue {
+class List_Q {
 	std::mutex mutex_;
 	std::list<T> q_;
+	size_t count;
+	size_t capacity;
+	std::condition_variable notFull notEmpty;
 	public : 
+		List_Q(size_t capacity=0): count(0), capacity(capacity);
+
 		void enq (T x) {
 			std::list <T> tmp;
 			tmp.push_back(std::move(x));
 			{
 				std::lock_guard<std::mutex> lock(mutex_);
-				q_.splice(end(q_), tmp);
+				q_.splice(end(q_), tmp);			//since we are inside lock, we want to do minimum work, finish fast. splice is constant time.
+			}
+		}
+
+		T deq (){
+			std::list <T> tmp;
+			{
+				std::lock_guard<std::mutex> lock(mutex_);
+				tmp.splice(tmp.begin(), list, list.begin());
 			}
 		}
 };
@@ -28,7 +42,7 @@ class concurrent_queue {
 
 
 int main (int argc, char* argv[]){
-	concurrent_queue<int> q;
+	List_Q<int> q;
 	std::thread t(&concurrent_queue<int>::enq, std::ref(q), 1);
 	t.join();
 }
